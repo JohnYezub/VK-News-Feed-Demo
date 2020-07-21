adf242d0a01aa36730a174beaaca3d10fb82ea8a
JohnYezub <buze@ya.ru>
2020-07-21 11:50:40 +0800

    Initial Commit

---
+++ VK News Feed Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK News Feed Demo
+//
+//  Created by   admin on 21.07.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
