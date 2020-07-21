4bd39c6ae24952ced86153e83b6fe541061284fe
JohnYezub <buze@ya.ru>
2020-07-21 18:00:27 +0800

    Setup Carthage. Add and configure VKSdk with user Auth. Add AuthViewController with login button. Add FeedViewController. Add AuthService class to handle auth methods with injection to SceneDelegate/AuthViewController/AddDelegate.

---
+++ VK News Feed Demo/FeedViewController/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  VK News Feed Demo
+//
+//  Created by   admin on 21.07.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
---
+++ VK News Feed Demo/Services/AuthService.swift
@@ -0,0 +1,72 @@
+//
+//  AuthService.swift
+//  VK News Feed Demo
+//
+//  Created by   admin on 21.07.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import Foundation
+import VKSdkFramework
+
+protocol AuthServiceDelegate: class {
+    func authServiceShouldShow(viewController: UIViewController)
+    func authServiceSignIn()
+    func authServiceSignInDidFail()
+}
+
+class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    private let appId = "7544876"
+    private let vkSdk: VKSdk
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appId)
+        super.init()
+        print(#function)
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    weak var delegate: AuthServiceDelegate?
+    
+    func wakeUpSession() {
+        let scope = ["offline"]
+        VKSdk.wakeUpSession(scope) { [delegate] (state, error) in
+            switch state {
+                
+            case .initialized:
+                print("initialized")
+                VKSdk.authorize(scope)
+            case .authorized:
+                print("authorized")
+                delegate?.authServiceSignIn()
+            default:
+                delegate?.authServiceSignInDidFail()
+            }
+        }
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+        if result != nil {
+            delegate?.authServiceSignIn()
+        }
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+        delegate?.authServiceSignInDidFail()
+    }
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+        delegate?.authServiceShouldShow(viewController: controller)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+    
+    
+}
