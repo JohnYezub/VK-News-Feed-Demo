68917d0b7b60a0679564e8fe8de285c6c14f2282
JohnYezub <buze@ya.ru>
2020-07-23 13:24:46 +0800

    Construct API in API sctruct, add NetworkService class, implement newsfeed get request

--- VK News Feed Demo/FeedViewController/FeedViewController.swift
+++ VK News Feed Demo/FeedViewController/FeedViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
+    private var networkService = NetworkService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         view.backgroundColor = .blue
+        
+        networkService.getFeed()
     }
 }
---
+++ VK News Feed Demo/Services/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  VK News Feed Demo
+//
+//  Created by   admin on 23.07.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import Foundation
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.120"
+    static let newsFeed = "/method/newsfeed.get"
+}
